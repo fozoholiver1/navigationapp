d2cd9da940b3a100e3a43b529c0f18650bfbfeb8
oliver <dopplereffect1992@gmail.com>
2020-06-04 20:48:06 +0100

    Add project files.

---
+++ navigationapp/Navigations.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace navigationapp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Navigations : TabbedPage
+    {
+        public Navigations()
+        {
+            InitializeComponent();
+        }
+    }
+}
